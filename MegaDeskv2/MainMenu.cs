5e64fd7ef20aed5d83f50ae8988851d1a523ef98
Brenda Wicker <bwicker@byui.edu>
2021-01-27 17:57:48 -0600

    Main menu

---
+++ MegaDeskv2/MainMenu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_Wicker
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+
+        }
+
+        //exit application
+        private void ButtonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        /// <summary>
+        /// go to AddQuote, hide MainMenu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonAddNewQuote_Click(object sender, EventArgs e)
+        {
+            AddQuote viewAddQuote = new AddQuote();
+            viewAddQuote.Tag = this;
+            viewAddQuote.Show(this);
+            Hide(); //hide MainMenu
+        }
+
+        /// <summary>
+        /// Go to ViewAllQuotes, hide MainMenu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonViewQuotes_Click(object sender, EventArgs e)
+        {
+            ViewAllQuotes viewAllQuotes = new ViewAllQuotes();
+            viewAllQuotes.Tag = this;
+            viewAllQuotes.Show(this);
+            Hide(); //hide MainMenu
+        }
+
+        /// <summary>
+        /// got to SearchQuotes, hide MainMenu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            SearchQuotes viewSearchQuotes = new SearchQuotes();
+            viewSearchQuotes.Tag = this;
+            viewSearchQuotes.Show(this);
+            Hide(); //hide MainMenu
+        }
+    }
+}
---
+++ MegaDeskv2/MainMenu.designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace MegaDesk_Wicker
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            this.ButtonAddNewQuote = new System.Windows.Forms.Button();
+            this.ButtonViewQuotes = new System.Windows.Forms.Button();
+            this.ButtonSearch = new System.Windows.Forms.Button();
+            this.ButtonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonAddNewQuote
+            // 
+            this.ButtonAddNewQuote.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonAddNewQuote.Location = new System.Drawing.Point(309, 12);
+            this.ButtonAddNewQuote.Name = "ButtonAddNewQuote";
+            this.ButtonAddNewQuote.Size = new System.Drawing.Size(95, 30);
+            this.ButtonAddNewQuote.TabIndex = 0;
+            this.ButtonAddNewQuote.Text = "&Add New Quote";
+            this.ButtonAddNewQuote.UseVisualStyleBackColor = true;
+            this.ButtonAddNewQuote.Click += new System.EventHandler(this.ButtonAddNewQuote_Click);
+            // 
+            // ButtonViewQuotes
+            // 
+            this.ButtonViewQuotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonViewQuotes.Location = new System.Drawing.Point(309, 54);
+            this.ButtonViewQuotes.Name = "ButtonViewQuotes";
+            this.ButtonViewQuotes.Size = new System.Drawing.Size(95, 30);
+            this.ButtonViewQuotes.TabIndex = 1;
+            this.ButtonViewQuotes.Text = "&View Quotes";
+            this.ButtonViewQuotes.UseVisualStyleBackColor = true;
+            this.ButtonViewQuotes.Click += new System.EventHandler(this.ButtonViewQuotes_Click);
+            // 
+            // ButtonSearch
+            // 
+            this.ButtonSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonSearch.Location = new System.Drawing.Point(309, 97);
+            this.ButtonSearch.Name = "ButtonSearch";
+            this.ButtonSearch.Size = new System.Drawing.Size(95, 30);
+            this.ButtonSearch.TabIndex = 2;
+            this.ButtonSearch.Text = "&Search";
+            this.ButtonSearch.UseVisualStyleBackColor = true;
+            this.ButtonSearch.Click += new System.EventHandler(this.ButtonSearch_Click);
+            // 
+            // ButtonExit
+            // 
+            this.ButtonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonExit.Location = new System.Drawing.Point(309, 140);
+            this.ButtonExit.Name = "ButtonExit";
+            this.ButtonExit.Size = new System.Drawing.Size(95, 30);
+            this.ButtonExit.TabIndex = 3;
+            this.ButtonExit.Text = "E&xit";
+            this.ButtonExit.UseVisualStyleBackColor = true;
+            this.ButtonExit.Click += new System.EventHandler(this.ButtonExit_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(468, 450);
+            this.Controls.Add(this.ButtonExit);
+            this.Controls.Add(this.ButtonSearch);
+            this.Controls.Add(this.ButtonViewQuotes);
+            this.Controls.Add(this.ButtonAddNewQuote);
+            this.Name = "MainMenu";
+            this.Text = "MainMenu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonAddNewQuote;
+        private System.Windows.Forms.Button ButtonViewQuotes;
+        private System.Windows.Forms.Button ButtonSearch;
+        private System.Windows.Forms.Button ButtonExit;
+    }
+}
+
