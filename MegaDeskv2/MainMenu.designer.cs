f16818a6056bec29e25f3e6d5abe07b847aeb5ca
Brenda Wicker <bwicker@byui.edu>
2021-01-28 14:00:46 -0600

    Wooden desk background

--- MegaDeskv2/MainMenu.designer.cs
+++ MegaDeskv2/MainMenu.designer.cs
@@ -45,10 +45,9 @@ namespace MegaDeskAngeles
             this.ButtonAddNewQuote.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
             this.ButtonAddNewQuote.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.ButtonAddNewQuote.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonAddNewQuote.Location = new System.Drawing.Point(13, 13);
-            this.ButtonAddNewQuote.Margin = new System.Windows.Forms.Padding(4);
+            this.ButtonAddNewQuote.Location = new System.Drawing.Point(25, 54);
             this.ButtonAddNewQuote.Name = "ButtonAddNewQuote";
-            this.ButtonAddNewQuote.Size = new System.Drawing.Size(127, 64);
+            this.ButtonAddNewQuote.Size = new System.Drawing.Size(104, 52);
             this.ButtonAddNewQuote.TabIndex = 0;
             this.ButtonAddNewQuote.Text = "&Add New Quote";
             this.ButtonAddNewQuote.UseVisualStyleBackColor = false;
@@ -57,10 +56,9 @@ namespace MegaDeskAngeles
             // ButtonViewQuotes
             // 
             this.ButtonViewQuotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonViewQuotes.Location = new System.Drawing.Point(13, 85);
-            this.ButtonViewQuotes.Margin = new System.Windows.Forms.Padding(4);
+            this.ButtonViewQuotes.Location = new System.Drawing.Point(25, 116);
             this.ButtonViewQuotes.Name = "ButtonViewQuotes";
-            this.ButtonViewQuotes.Size = new System.Drawing.Size(127, 37);
+            this.ButtonViewQuotes.Size = new System.Drawing.Size(104, 30);
             this.ButtonViewQuotes.TabIndex = 1;
             this.ButtonViewQuotes.Text = "&View Quotes";
             this.ButtonViewQuotes.UseVisualStyleBackColor = true;
@@ -69,10 +67,9 @@ namespace MegaDeskAngeles
             // ButtonSearch
             // 
             this.ButtonSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonSearch.Location = new System.Drawing.Point(13, 130);
-            this.ButtonSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.ButtonSearch.Location = new System.Drawing.Point(25, 155);
             this.ButtonSearch.Name = "ButtonSearch";
-            this.ButtonSearch.Size = new System.Drawing.Size(127, 37);
+            this.ButtonSearch.Size = new System.Drawing.Size(104, 30);
             this.ButtonSearch.TabIndex = 2;
             this.ButtonSearch.Text = "&Search";
             this.ButtonSearch.UseVisualStyleBackColor = true;
@@ -81,10 +78,9 @@ namespace MegaDeskAngeles
             // ButtonExit
             // 
             this.ButtonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonExit.Location = new System.Drawing.Point(13, 175);
-            this.ButtonExit.Margin = new System.Windows.Forms.Padding(4);
+            this.ButtonExit.Location = new System.Drawing.Point(25, 194);
             this.ButtonExit.Name = "ButtonExit";
-            this.ButtonExit.Size = new System.Drawing.Size(127, 37);
+            this.ButtonExit.Size = new System.Drawing.Size(104, 30);
             this.ButtonExit.TabIndex = 3;
             this.ButtonExit.Text = "E&xit";
             this.ButtonExit.UseVisualStyleBackColor = true;
@@ -92,15 +88,14 @@ namespace MegaDeskAngeles
             // 
             // MainMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(633, 573);
+            this.ClientSize = new System.Drawing.Size(475, 425);
             this.Controls.Add(this.ButtonExit);
             this.Controls.Add(this.ButtonSearch);
             this.Controls.Add(this.ButtonViewQuotes);
             this.Controls.Add(this.ButtonAddNewQuote);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainMenu";
             this.Text = "MainMenu";
             this.ResumeLayout(false);
