7c76fbc7855039b70f2998759f4603ab8b475422
cris-rr <59718617+cris-rr@users.noreply.github.com>
2021-02-02 00:17:03 -0500

    Struct to display selected columns Search Quotes working

--- MegaDeskv2/DeskQuote.cs
+++ MegaDeskv2/DeskQuote.cs
@@ -134,15 +134,15 @@ namespace MegaDeskAngeles
 
     public struct Quotes
     {
-        public string customerName;
-        public string quoteDate;
-        public string rushOption;
-        public double totalCost;
-        public int width;
-        public int depth;
-        public int drawers;
-        public string material;
-        public int area;
+        public string customerName { get; set; }
+        public string quoteDate { get; set; }
+        public string rushOption { get; set; }
+        public double totalCost { get; set; }
+        public int width { get; set; }
+        public int depth { get; set; }
+        public int drawers { get; set; }
+        public string material { get; set; }
+        public int area { get; set; }
     }
 
 }
--- MegaDeskv2/MainMenu.cs
+++ MegaDeskv2/MainMenu.cs
@@ -22,7 +22,7 @@ namespace MegaDeskAngeles
             string json = File.ReadAllText("quotes.json");
             try
             {
-                string json = File.ReadAllText("quotes.json", System.Text.Encoding.UTF8);
+                json = File.ReadAllText("quotes.json", System.Text.Encoding.UTF8);
                 AllTheQuotes = JsonConvert.DeserializeObject<List<DeskQuote>>(json);
                 Console.WriteLine("revisar json");
             }
