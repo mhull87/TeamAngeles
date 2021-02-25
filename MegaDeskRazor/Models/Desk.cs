0a2f2679404a8f0a1adfe4d1cd99cc6b77f7db13
Melissa Hull <melissahull87@gmail.com>
2021-02-25 17:26:41 -0600

    css fixes and formatting $ and inches

---
+++ MegaDeskRazor/Models/Desk.cs
@@ -0,0 +1,41 @@
+﻿namespace MegaDeskRazor.Models
+{
+    //   public enum DesktopMaterial
+    //   {
+    //       Oak,
+    //       Laminate,
+    //       Pine,
+    //       Rosewood,
+    //       Veneer
+    //    }
+
+    //this class will be deleted, it is for reference only
+    public class Desk
+    {
+        public int Width { get; set; }
+        public int Depth { get; set; }
+        public int Drawers { get; set; }
+        public DesktopMaterial Material { get; set; }
+        public int area;
+
+        //Desk Constraints
+        public const int MINWIDTH = 24;
+        public const int MAXWIDTH = 96;
+        public const int MINDEPTH = 12;
+        public const int MAXDEPTH = 48;
+
+        public Desk()
+        {
+            Width = 0;
+            Depth = 0;
+            Drawers = 0;
+            Material = 0;
+        }
+
+        public int CalculateArea()
+        {
+            area = Width * Depth;
+            return area;
+        }
+    }
+}
