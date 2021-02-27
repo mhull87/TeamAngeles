1fc2a392e12d73d7ade21462b6c401d55aeb65d5
cris-rr <59718617+cris-rr@users.noreply.github.com>
2021-02-27 11:59:48 -0500

    Matrial images dropbox in Create and edit

--- MegaDeskRazor/wwwroot/js/site.js
+++ MegaDeskRazor/wwwroot/js/site.js
@@ -2,13 +2,43 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-var width = document.getElementById("width").innerHTML;
-var depth = document.getElementById("depth").innerHTML;
-var calc = document.getElementById("depth").onkeyup(CalculateArea(width, depth));
-document.getElementById("area").value = calc;
+//var width = document.getElementById("width").innerHTML;
+//var depth = document.getElementById("depth").innerHTML;
+//var calc = document.getElementById("depth").onkeyup(CalculateArea(width, depth));
+//document.getElementById("area").value = calc;
 
 function CalculateArea(width, depth)
-    {
+{
         area = width * depth;
         return area;
+}
+
+// To add images to dropbox materials --------------------------------
+window.onload = function () {
+    const routes = ["/Quotes/Edit", "/Quotes/Create"];
+    const pathUrl = window.location.pathname;
+    
+    if (routes.includes(pathUrl)) {
+        console.log(pathUrl);
+        addImages();
+    } 
+}
+//window.onload = addImages();
+
+function addImages() {
+    const newURL = window.location.protocol + "//" + window.location.host + "/";
+    const materialSelect = document.querySelector('.materialSelect');
+    const options = materialSelect.options;
+
+    for (let i = 0; i < options.length; i++) {
+        let name = options[i].value;
+        let src = `${newURL}lib/images/${name}.jpg`;
+        //console.log(src);
+        options[i].setAttribute("data-left" ,src);
     }
+    $('.materialSelect').selectator();
+}
+// ----------------------------------------------------------
+
+
+
